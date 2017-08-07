c09ceb55124796fcba537331317a4486754fc06c
Mateusz Koszutowski <mkoszutowski@divante.pl>
2017-08-07 13:49:21 +0200

    Fixing conf for sharing classes dir

--- lib/capistrano/pimcore5/defaults.rb
+++ lib/capistrano/pimcore5/defaults.rb
@@ -4,7 +4,7 @@ set :linked_files, fetch(:linked_files, []).push
 
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'vendor',
-  'var/classes',
+  'var/classes/Object',
   'var/config',
   'var/log',
   'var/recyclebin',
