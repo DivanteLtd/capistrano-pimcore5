47be0a1dae5f829a3d1b3086659791bda295507b
Mateusz Koszutowski <mkoszutowski@divante.pl>
2017-11-02 16:41:27 +0100

    Fixing code style

--- lib/capistrano/pimcore5/defaults.rb
+++ lib/capistrano/pimcore5/defaults.rb
@@ -2,10 +2,10 @@ set :env, 'development'
 set :pimcore_deploy_classes_rebuild, fetch(:pimcore_deploy_classes_rebuild, true)
 
 set :linked_files, fetch(:linked_files, []).push(
-	'var/config/system.php',
-	'var/config/debug-mode.php',
+  'var/config/system.php',
+  'var/config/debug-mode.php',
   'var/config/extensions.php',
-	'app/config/parameters.yml',
+  'app/config/parameters.yml'
 )
 
 set :linked_dirs, fetch(:linked_dirs, []).push(
